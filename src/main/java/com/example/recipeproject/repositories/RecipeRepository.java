47d8ce8a17a3b6fb63a39f98f7eb9a6a8be61f2b
Renchiiks <renate.kalmikova@gmail.com>
2020-10-16 12:18:00 +0300

    Added Repository interfaces

---
+++ src/main/java/com/example/recipeproject/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package com.example.recipeproject.repositories;
+
+import com.example.recipeproject.model.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
