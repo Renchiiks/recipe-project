2ff7883d8f2c5d514ec95fd6d0cba5d94e521fd3
Renchiiks <renate.kalmikova@gmail.com>
2020-10-16 12:09:45 +0300

    Added Getters and Setters for Entities

--- src/main/java/com/example/recipeproject/model/Ingredient.java
+++ src/main/java/com/example/recipeproject/model/Ingredient.java
@@ -17,4 +17,44 @@ public class Ingredient {
 
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure measure;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(UnitOfMeasure measure) {
+        this.measure = measure;
+    }
 }
