1f230e0edd4d3a11c382e3feec4fd50c2d70f2ad
Renchiiks <renate.kalmikova@gmail.com>
2020-10-20 18:16:51 +0300

    Creating integration test for UnitsOfMeasure

---
+++ src/test/java/com/example/recipeproject/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,41 @@
+package com.example.recipeproject.repositories;
+
+import com.example.recipeproject.model.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    //@DirtiesContext
+    void findByUom() {
+
+        Optional<UnitOfMeasure> measure = unitOfMeasureRepository.findByUom("Each");
+
+        assertEquals("Each", measure.get().getUom());
+    }
+
+    @Test
+    void findByUomCup() {
+
+        Optional<UnitOfMeasure> measure = unitOfMeasureRepository.findByUom("Cup");
+
+        assertEquals("Cup", measure.get().getUom());
+    }
+}
